84367225d0bfcbed64cdbdfdf77615131b3caead
Pravish Bajpai <55047219+pravishbajpai06@users.noreply.github.com>
2020-08-28 19:08:18 +0530

    Create runnerup.py

---
+++ runnerup.py
@@ -0,0 +1,14 @@
+'''
+Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given  scores. 
+Store them in a list and find the score of the runner-up.
+
+Sample Input 0
+
+5
+2 3 6 6 5
+Sample Output 0
+
+5'''
+
+
+n=int(input())
